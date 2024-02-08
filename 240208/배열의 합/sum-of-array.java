90c5033199edc7293ee9101c4a835e14ccc0becd
Jeonghwa Heo <48192141+hoa0217@users.noreply.github.com>
2024-02-08 12:51:46 +0900

    Title: 배열의 합, Time: 153ms, Memory: 10MB, Status: Passed - CodeTree

--- 240208/배열의 합/sum-of-array.java
+++ 240208/배열의 합/sum-of-array.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = 4
+        int n = 4;
         for(int i = 0; i< n; i++){
             int sum = 0;
             for(int j = 0; j < n; j++){
